04a868065c03d641cef52f4b6c5c6f20c3963b58
Akshay-a-j <38350400+Akshay-a-j@users.noreply.github.com>
2021-04-30 11:23:16 +0530

    Day 2: string

--- Codes/Day_002.py
+++ Codes/Day_002.py
@@ -19,7 +19,7 @@ the same way he originally typed them.
 
 Example:
 Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
-Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real" '''
+Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real " '''
 
 def to_jaden_case(string):
     print(string)
